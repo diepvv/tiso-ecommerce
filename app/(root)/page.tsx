600e090a3513cff6d6ccf919bae204549d37ec97
diepvv <diepvu1995@gmail.com>
2025-03-05 16:02:52 +0700

    create sample product and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
   return (
     <>
-      <Button>Button</Button>
+    <ProductList data={sampleData.products} title="Newest Arrival" limit={4}/>
     </>
   );
 }
