813616908c135f0b3b99e0740efcc5ffa58a143f
diepvv <diepvu1995@gmail.com>
2025-03-24 23:39:13 +0700

    add cart page

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Cart } from "@/types";
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+    </>
+  );
+};
+
+export default CartTable;
